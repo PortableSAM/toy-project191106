148bf233f890d93c09a9f04ce7cbb57014ee30e1
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-07 18:39:22 +0900

    Main.js Delete

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link, BrowserRouter as Router } from "react-router-dom";
-import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+//import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import "./App.css";
 import "./normalize.css";
 // Main Component
-import Main from "./components/Main";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="demo-big-content">
-        <Layout>
-          <Header title="Title" scroll>
-            <Navigation>
-              <Router>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-              </Router>
-            </Navigation>
-          </Header>
-          <Drawer title="Title">
-            <Navigation>
-              <Router>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-              </Router>
-            </Navigation>
-          </Drawer>
-          <Content>
-            <div className="page-content" />
-            <Main />
-          </Content>
-        </Layout>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="contact">Contact</Link>
+      </header>
+      <hr />
+      <Main />
+    </Router>
+  );
 }
 
 export default App;

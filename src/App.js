c6e6e313ef0b7452d5b7a07a119a2b952a6437a1
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-07 19:05:05 +0900

    Layout, CSS Modification

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from "react";
-import { Link, BrowserRouter as Router } from "react-router-dom";
-//import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import "./App.css";
 import "./normalize.css";
-// Main Component
+// Component
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <Router>
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="contact">Contact</Link>
-      </header>
-      <hr />
-      <Main />
+      <div className="demo-big-content">
+        <Layout>
+          <Header title="Title" scroll>
+            <Navigation>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="content-page">
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/projects" component={Projects} />
+              <Route exact path="/contact" component={Contact} />
+            </div>
+          </Content>
+        </Layout>
+      </div>
     </Router>
   );
 }

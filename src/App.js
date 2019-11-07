bc53a787031a0469eb432def7b10d355fd79181e
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-07 18:13:46 +0900

    import react-mdl

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link, BrowserRouter as Router } from "react-router-dom";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import "./App.css";
 import "./normalize.css";
 // Main Component
@@ -8,16 +9,33 @@ import Main from "./components/Main";
 class App extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <Router>
-            <Link to="/">Home</Link>
-            <Link to="/About">About</Link>
-            <Link to="/Projects">Projects</Link>
-            <Link to="/Contact">Contact</Link>
-          </Router>
-        </header>
-        <Main />
+      <div className="demo-big-content">
+        <Layout>
+          <Header title="Title" scroll>
+            <Navigation>
+              <Router>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+              </Router>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <Router>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+              </Router>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
       </div>
     );
   }

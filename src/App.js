d6c4cd4b3b2951a7c22c097ff4e544d62a650b66
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-07 18:04:13 +0900

    Create Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import "./normalize.css";
+// Main Component
+import Main from "./components/Main";
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return <div>HaHaha</div>;
+    return (
+      <div>
+        <header>
+          <Router>
+            <Link to="/">Home</Link>
+            <Link to="/About">About</Link>
+            <Link to="/Projects">Projects</Link>
+            <Link to="/Contact">Contact</Link>
+          </Router>
+        </header>
+        <Main />
+      </div>
+    );
   }
 }
 

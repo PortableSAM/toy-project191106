194e4d68e243a69590f8917882f52c551a2a2848
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-07 16:49:14 +0900

    Add normalize.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
+import "./normalize.css";
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>Hello React!</h1>
-      </div>
-    );
+    return <div>HaHaha</div>;
   }
 }
 
